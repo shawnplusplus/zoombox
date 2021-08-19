58f970b6c0aa1bf6ef373a07b6575867111d857a
shawnplusplus <46225246+shawnplusplus@users.noreply.github.com>
2021-08-18 20:37:44 -0500

    Learning JavaScript

---
+++ example.js
@@ -0,0 +1,51 @@
+// var chameleon = "blue";
+// alert(chameleon);
+
+
+// var rack = [];
+// rack[0] = "Shawn";
+// rack[1] = "Katie";
+// rack[2] = "Harrison";
+// rack[3] = "Charlie";
+// rack[4] = "Tucker";
+// alert(rack);
+
+// var subArray1 = ["Paris", "Lyon", "Nice"];
+// var subArray2 = ["Amsterdam", "Eindhoven", "Utrecht"];
+// var subArray3 = ["Madrid", "Barcelona", "Seville"];
+
+// var superArray = [subArray1, subArray2, subArray3];
+// var city = "";
+// city = superArray [0][2]
+// alert(city);
+
+// var age = 17;
+// if (age > 20) 
+// {
+//     alert("Drink to get drunk.");
+// }
+// else 
+// (
+//         alert("Stop at the door.")
+// )
+
+// var numbers = [1, 2, 3, 4, 5];
+// var incrementer = 0;
+// while (incrementer < numbers.length)
+// {
+//     numbers[incrementer] *= 2;
+//     incrementer++;
+// }
+
+var numbers = [];
+numbers[0] = 0;
+numbers[1] = 1;
+
+
+function adding(_mem1, _mem2)
+{
+    var sum = 0;
+    sum = _mem1 + _mem2;
+    
+}
+adding(1, 2, 3);
